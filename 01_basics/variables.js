02fb2d421d3d9ad1bf1ad80281f173ed4e5043bd
Amol8739 <ashiraskar63@gmail.com>
2026-01-01 15:00:53 +0000

    Changed

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,19 +1,31 @@
-const accountId = 1212
+const accountId = 121212
+// constants cannot be changed once Declared.
+
 let accountEmail = "amol21@google.com"
+
+
 var accountPassword = "12345"
-accountCity = "Daund"
-let accountState
-
-// accountId = 21 //not allowed
-accountEmail = "amol@google.com"
-accountPassword = "2121"
-accountCity = "Pune"
-
 /*
+Variables can be changed in further code.
 Prefer not to use var 
 because of issue in block scope and funtional scope
 */
 
-console.log(accountId);
-console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
 
+accountCity = "Daund"
+// Variables can also be declared like this also. But this method is not prefered.
+
+let accountState
+
+//Lets try to make some change in its initial values.
+
+// accountId = 21 //not allowed
+accountEmail = "amol@google.com"  // Can be changed 
+accountPassword = "54321"  // Can be changed
+accountCity = "Pune" // Can be changed
+
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
+//Instead of executing each and every variable you can use table to show in table format
