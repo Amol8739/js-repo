801766be93df8bc527b3e39a1e83fe7f25f9d6d2
Amol8739 <ashiraskar63@gmail.com>
2026-01-01 15:41:00 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+//"use strict"; // Treats all JS Code as a newer version.
+
+//alert(3 + 3) // alert is only used in browser not in nodejs.
+
+console.log(3 
+    + 3) // Code readability should be high.
+
+// Primitive Datatypes 
+
+let age = 12 // Numerical datatype Range = upto 2^53 
+let  = "Amol" // String datatype
+let loggenIn = true // Boolean Datatype
+// null => It is Standalone Value
+// bigint => to store big number 
+//undefined => value is not assigned
+//symbol => To find uniqueness in component or figma tools
+
+console.log(typeof age)
+console.log(typeof "Amol")
+console.log(typeof loggenIn)
+console.log(typeof null) // Output is Object
+console.log(typeof undefined) // Output is undefined
