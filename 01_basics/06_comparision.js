da1239805afced89c17a7977b898b5bcfa4d273e
Amol8739 <ashiraskar63@gmail.com>
2026-01-03 16:44:25 +0000

    Comparisions

---
+++ 01_basics/06_comparision.js
@@ -0,0 +1,35 @@
+// console.log(1 > 2);
+// console.log(1 >= 2);
+// console.log(1 <= 2);
+// console.log(1 == 2);
+// console.log(1 != 2);
+
+//console.log("1" < 2); // Even though the answer is true.
+// Always use same Datatype for comparision it is important for TypeScript
+
+// console.log(null < 0);
+// console.log(null > 0);
+// console.log(null <= 0); // 0 is greater than equal to to null but not greater than 0.
+// console.log(null >= 0); // 0 is also less than equal to null but not less than 0
+// console.log(null == 0);
+// console.log(null != 0); // And also 0 is not equal to null.
+
+
+// This types of comparision can make you so confusing 
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined != 0); // Undefined is not equal to 0. This is true
+
+// console.log(null < undefined);
+// console.log(null > undefined);
+// console.log(null <= undefined);
+// console.log(null >= undefined);
+// console.log(null == undefined); // Null is equal to undefined. This is true.
+// console.log(null != undefined);
+
+// Strict Check "===" this not only check values but also strictly checks Datatypes 
+
+console.log("2" === 2); // Output is false.
