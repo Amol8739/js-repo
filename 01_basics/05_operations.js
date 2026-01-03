1cd605e570490ddb1bfefaea101420a6a99b1bab
Amol8739 <ashiraskar63@gmail.com>
2026-01-03 15:34:34 +0000

    Operations

---
+++ 01_basics/05_operations.js
@@ -0,0 +1,39 @@
+// let val = 4
+// let negval = - val
+
+// console.log(negval)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(8/2)
+// console.log(9%2)
+
+// let str1 = "Amol"
+// let str2 = " Shiraskar"
+
+// console.log(str1 + str2);
+
+// OR  
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// Don't write code like this it is worst pattern to write. So avoid using this type of code.
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// If String is first then it treats whole operation as a String
+// console.log(1 + 2 + "3"); 
+// If String is last then operation is done until String appear then treats as a String.
+// console.log(+ true);
+// console.log(+"1");
+
+let gameCounter = 100
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
+
