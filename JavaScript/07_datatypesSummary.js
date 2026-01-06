0f3ddbc0ae25df01b4cda5051143b67c8037b2f5
Amol8739 <ashiraskar63@gmail.com>
2026-01-06 04:09:45 +0000

    Summary

---
+++ JavaScript/07_datatypesSummary.js
@@ -0,0 +1,70 @@
+/* Data Types is divided into 2 types on the basis of how the data is stored in the memory and 
+how to access it. 
+
+1) Primitive Datatype
+
+- Stores single value
+- Immutable (cannot be changed directly)
+- Stored by value
+- Faster and memory efficient
+- Primitive Datatype are call by value means that When you copy datatype the the original
+  reference is not used it is only copied. 
+- There are 7 types.
+1) String
+2) Number
+3) Boolean
+4) Null
+5) Undefined
+6) Symbol
+7) BigInt
+*/
+
+// Symbol Example :
+// let id = Symbol('123')
+// let anotherId = Symbol('123')
+
+// console.log(id);
+// console.log(anotherId);
+// console.log(id === anotherId);
+
+// BigInt Example :
+// let bigInt = 18431546325185431315n // n is used for Big Numbers 
+// console.log(bigInt);
+
+
+
+
+/*
+2) Non - Primitive or Reference Datatype
+
+- Can store multiple values
+- Mutable
+- Stored by reference
+- Can have methods & properties
+- Types of Non - Primitive Datatype
+1) Array
+2) Objects
+3) Funtions
+*/
+//Array Example :
+
+let heros = ["Superman", "Batman", "Ironman"]
+
+//Object Example :
+
+let obj = {
+    name : "Amol",
+    age : 55,
+    class : "BE"
+}
+
+//Function Example :
+
+const myFirstFunc = function () {
+  console.log("Hello World");
+}
+
+console.log(typeof heros); // Output is Object
+console.log(typeof obj);  // Output is Object
+console.log(typeof myFirstFunc); // Output is Function but actually it is ObjectFunction.
+
